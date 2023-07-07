ed07cbc1beb415cd0c99c9492f2e07aaca273e81
chokinyan <55279869+chokinyan@users.noreply.github.com>
2023-07-07 20:54:25 +0000

    embed r34

--- test3/testdejs/commandes/r34.js
+++ test3/testdejs/commandes/r34.js
@@ -1,4 +1,4 @@
-const {SlashCommandBuilder} = require("discord.js");
+const {SlashCommandBuilder,EmbedBuilder} = require("discord.js");
 const request = require('request');
 
 module.exports = {
@@ -14,9 +14,9 @@ module.exports = {
 
     async excute(interaction){
         const nb = Math.floor(Math.random()*1000);
+        const who = Math.floor(Math.random()*nb);
         let cat = interaction.options.getString('categoris');
         for(let i=0;i<cat.length;i++){cat = cat.replace(" ","_")};
-        console.log(cat);
 
         request.get({url : `https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&tags=${cat}~&json=1&limit=${nb}`},async (err,_rep,body)=>{
             if(err){
@@ -28,8 +28,12 @@ module.exports = {
                 }
                 else{
                     const resultat = JSON.parse(body);
-                    await interaction.reply({content : `${resultat[0]?.file_url}`});
-                }
+                    const embed = new EmbedBuilder()
+                    .setAuthor({name : `${resultat[who].owner}`})
+                    .setImage(`${resultat[who]?.file_url}`)
+                    .setFooter({text : `search for ${cat}`})
+                    await interaction.reply({embeds : [embed]});
+                };
             };
         });
     }
