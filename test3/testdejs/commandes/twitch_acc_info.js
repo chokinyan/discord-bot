09f4fa260d3ee7d8fc0a68a3b5b894349b591c74
chokinyan <55279869+chokinyan@users.noreply.github.com>
2023-07-10 14:56:58 +0000

    twitch search channel

--- test3/testdejs/commandes/twitch_acc_info.js
+++ test3/testdejs/commandes/twitch_acc_info.js
@@ -1,4 +1,4 @@
-const {SlashCommandBuilder} = require("discord.js");
+const {SlashCommandBuilder,EmbedBuilder} = require("discord.js");
 const {gettoken} = require('../donné & autre/tokentw');
 const {twitch_id} = require('../donné & autre/config.json');
 const request = require('request');
@@ -20,14 +20,29 @@ module.exports = {
             headers : {
                 Authorization : `Bearer ${token}`,
                 "Client-Id" : twitch_id
-            }
+            },
+            json : true
         };
         request.get(para,async (err,_repp,body)=>{
             if(err){
                 console.error(err);
             }
             else{
-                await interaction.reply({content : `${body}`});
+                console.log(body);
+                if(body.data.length === 0){
+                    await interaction.reply({content : "channel not found !",ephemeral : true})
+                }
+                else{
+                    const result = body.data[0];
+                    const part = !(result.broadcaster_type === ""); 
+                    const emb = new EmbedBuilder()
+                    .setAuthor({name : `${result.display_name}`,url : `https://www.twitch.tv/${result.display_name}`})
+                    .setColor('Purple')
+                    .setImage(`${result.profile_image_url}`)
+                    .setDescription(`${result?.description}`)
+                    .setFooter({text : `twitch partenaire : ${part}`});
+                    await interaction.reply({embeds : [emb]});
+                }
             }
         });
     }
