0e9116ce858d193b11bb5f162f01b31fa4c66231
chokinyan <sabou2344@gmail.com>
2023-08-03 11:19:33 +0200

    test

---
+++ test3/reachpres/Untitled-1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <audioclient.h>
+using namespace std;
+
+int main(){
+    cout << "aud" << endl;
+    cout << "test";
+    return 0;
+}
--- test3/reachpres/index.js
+++ test3/reachpres/index.js
@@ -1,32 +1,38 @@
 const discordR = require('discord-rpc');
+const fs = require('fs');
 const client_id = '1130870011909656586';
 const clientSecret = "uAw87DHzsnHXy7vq2bH8UIryVXpJU5EU";
 const rich = new discordR.Client({transport : 'ipc'});
 discordR.register(client_id);
 const acti = async ()=>{
+    const det = await JSON.parse(fs.readFileSync('./test3/reachpres/url.json','utf-8'));
     if(!rich){
         console.log("perso err");
         return;
     };
     rich.setActivity({
-    details : 'test',
-    state : 'test 2',
-    startTimestamp : Date.now(),
-    largeImageKey : 'image_2023-07-18_163423094',
-    largeImageText : 'test img.',
-    instance : false,
-    buttons : [
-        {
-            label : 'test B',
-            url : 'https://www.youtube.com/watch?v=DGmK_cN9370'
-        }
-    ]
-})};
+        details : `Ecoute de la musique de weeb sur ytb`,
+        state : `ecoute ${det.detail}`,
+        startTimestamp : Date.now(),
+        largeImageKey : 'image_2023-07-18_163423094',
+        largeImageText : 'test img.',
+        instance : false,
+        buttons : [
+            {
+                label : "lien de la musique",
+                url : det.url
+            }
+        ]
+    });
+};
 
 rich.on('ready',()=>{
     console.log("ready");
     acti();
-});
 
+    setInterval(()=>{
+        acti();
+    },30000);
+});
 
 rich.login({clientId : client_id,clientSecret : clientSecret}).catch(err => console.error(err));
